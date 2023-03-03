868ccfa176b4b33cffba7fb037be983d634d5174
thereconjacob <jvpokeyjake@gmail.com>
2023-03-03 11:44:21 +0000

    Do exercise 5

--- BucksMemoryApp.cpp
+++ BucksMemoryApp.cpp
@@ -44,14 +44,33 @@ void MemoryErrorHandler() {
 	exit(1);
 };
 
+//Functions need to be declared in advance so they're both initialised before they call one another
+void FunctionBCallsA();
+void FunctionACallsB();
+
+void FunctionACallsB() {
+	FunctionBCallsA();
+};
+
+void FunctionBCallsA() {
+	FunctionACallsB();
+};
+
 int main() {
 	AIManager* ai = new AIManager(10);
 	std::set_new_handler(MemoryErrorHandler);
-	for(int i = 1; i > 0; i++)
-	{
-		char* MemoryString = new char[512000];
-		std::cout << sizeof(MemoryString) << endl;
-	}
+
+	FunctionACallsB();
+	//Having the array as static prevented the stack overflow from happening somehow
+	//int arr[1000000];
+
+	//signed long int matrix[1201][1201] = { 0 };
+
+	//for(int i = 1; i > 0; i++)
+	//{
+	//	char* MemoryString = new char[512000];
+	//	std::cout << sizeof(MemoryString) << endl;
+	//}
 	ai->Add(0);
 	ai->Add(1);
 	ai->Add(2);
