2f6dd1dc985943e778db934996392f638f92f145
thereconjacob <jvpokeyjake@gmail.com>
2023-03-03 11:14:33 +0000

    Do exercise 3

--- BucksMemoryApp.cpp
+++ BucksMemoryApp.cpp
@@ -8,13 +8,12 @@ public:
 };
 class AIManager {
 private:
-	NPC** npcs;
+	NPC** npcs = NULL;
 	int count;
 	int size;
 public:
 	AIManager(int size) :size(size) {
-		if(size > 0)
-		npcs = new NPC * [size];
+		if(size > 0) npcs = new NPC * [size];
 		count = 0;
 	}
 	int Add(int id) {
@@ -39,8 +38,20 @@ public:
 		npcs = 0;
 	}
 };
+
+void MemoryErrorHandler() {
+	cerr << "You have run out of memory! \n";
+	exit(1);
+};
+
 int main() {
 	AIManager* ai = new AIManager(10);
+	std::set_new_handler(MemoryErrorHandler);
+	for(int i = 1; i > 0; i++)
+	{
+		char* MemoryString = new char[512000];
+		std::cout << sizeof(MemoryString) << endl;
+	}
 	ai->Add(0);
 	ai->Add(1);
 	ai->Add(2);
